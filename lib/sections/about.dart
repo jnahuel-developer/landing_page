6558fac98e920d5cb1aafe258dfe8b47bcb42b1b
Nahuel Martinez <jnahuel.developer@gmail.com>
2025-09-17 19:43:45 -0300

    Se arregla el about

--- lib/sections/about.dart
+++ lib/sections/about.dart
@@ -17,10 +17,9 @@ class AboutMeSection extends StatelessWidget {
           Text(
             "Soy Ingeniero Electrónico con más de 6 años de experiencia entre la industria médica y la industria financiera."
             "Me focalizo en las mejoras de producto, buscando aumentar la calidad con reducción de costos productivos."
-            "He desarrollado y puesto en comercialización el “Purity UV”, así como más de 10 proyectos internos."
+            "He desarrollado y puesto en comercialización el “Purity UV”, así como más de 10 proyectos internos.\n\n"
             "Reduje hasta 20% los costos productivos de electrónica, hasta un 30% el retrabajo por fallas comunes,"
-            "y hasta un 50% el tiempo de calibración y control de los equipos."
-            
+            "y hasta un 50% el tiempo de calibración y control de los equipos.\n\n"
             "Trabajo en desarrollo de aplicaciones móviles y web con Flutter, "
             "aplicaciones de escritorio en C/C++ con interfaces gráficas, "
             "bases de datos SQL, diseño de PCBs, integración IoT y redacción de informes técnicos.\n\n"
